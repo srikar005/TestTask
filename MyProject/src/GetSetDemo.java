0f92bad8e2af2204cf170675a918ac0bb8e54c63
HP <HP@LAPTOP-7VA8FTN7>
2019-12-03 14:41:25 +0530

    This is my first trial project from eclipse.

---
+++ MyProject/src/GetSetDemo.java
@@ -0,0 +1,16 @@
+
+public class GetSetDemo {
+String MyBankName;
+int MyBankAccount;
+public String getMyBankName()
+}
+public int getMyBankAccount() {
+	return MyBankAccount;
+}
+public void setMyBankAccount(int myBankAccount) {
+	MyBankAccount = myBankAccount;
+}
+public void setMyBankName(String myBankName) {
+	MyBankName = myBankName;
+}
+}
